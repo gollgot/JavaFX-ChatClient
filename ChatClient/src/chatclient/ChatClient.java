2c1dc78c6031685ce5e00e561f4a5d018d1b4e68
gollgot <loic.dessaules@gmail.com>
2016-11-17 23:04:28 +0100

    Created a new layout design
    I created a new layout design

--- ChatClient/src/chatclient/ChatClient.java
+++ ChatClient/src/chatclient/ChatClient.java
@@ -22,7 +22,7 @@ public class ChatClient extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
--- ChatClient/src/chatclient/FXMLDocumentController.java
+++ ChatClient/src/chatclient/FXMLDocumentController.java
@@ -5,14 +5,21 @@
  */
 package chatclient;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 
 /**
  *
@@ -20,6 +27,10 @@ import javafx.scene.control.TextField;
  */
 public class FXMLDocumentController implements Initializable {
     
+    
+    
+    
+    @FXML
     private TextArea taContent;
     @FXML
     private TextField tfMessage;
@@ -36,18 +47,24 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Button btnSend;
     
+    // Place the "cursor" of the text area at the botom of it
+    private void goToTheEndOfTheTextAreaContent(){
+        taContent.positionCaret(taContent.getLength());
+    }
     
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    public void initialize(URL url, ResourceBundle rb){
+        btnDisconnection.setVisible(false);
+        btnSend.setDisable(true);
     }    
 
     @FXML
-    private void btnConnexionAction(ActionEvent event) {
+    private void btnConnexionAction(ActionEvent event){
     }
 
     @FXML
     private void btnDisconnexionAction(ActionEvent event) {
+      
     }
     
 }
